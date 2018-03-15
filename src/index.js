af5f2bd0a4ad9eb10b8edb740c006d546fcac8da
Pavel <pahonski@gmail.com>
2018-03-15 17:16:31 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        // your implementation
+        this.array = [initialValue];
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        // your implementation
+        this.array.push('+', number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        // your implementation
+        this.array.push('-', number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        // your implementation
+        this.array.push('*', number);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        // your implementation
+        this.array.push('/', number);
+        return this;
+    }
+
+    pow(number) {
+        // your implementation
+        this.array.push('^', number);
+        return this;
+    }
+
+    result() {
+        console.log(this.array);
+        return this.array;
+    }
+
+
 }
+
+let example = new SmartCalculator(2);
+example.add(2);
+example.multiply(2);
+example.result();
 
 module.exports = SmartCalculator;
